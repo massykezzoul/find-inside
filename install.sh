e6c7387714d2511f0962f12b5505c03b4d982edf
Massili Kezzoul <massy.kezzoul@gmail.com>
2019-03-31 23:50:03 +0200

    Installation

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## This script copy the file "findall.py" to the directory bin/ on your HOME dir
+## and create a symbolic link called findall
+## so that you can use it from any directory like this : `findall [dir] "KetWord"`
+## if ~/bin/ not exists it will be created
+
+if [ ! -d $HOME/bin/ ] 
+then
+    mkdir $HOME/bin/
+fi
+
+cp findall.py $HOME/bin/.
+ln -s $HOME/bin/findall.py $HOME/bin/findall
+
+## Massy Kezzoul
+## github -> https://github.com/massykezzoul/find-inside
